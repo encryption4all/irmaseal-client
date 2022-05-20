3979ccb3dc50f8a9703ac20201a00abd0e170a87
Leon <l.botros@cs.ru.nl>
2022-05-20 17:47:32 +0200

    update example

--- webpack.config.js
+++ webpack.config.js
@@ -7,53 +7,51 @@ const dist = path.resolve(__dirname, 'dist')
 const webpackMode = 'development'
 
 module.exports = {
-  name: 'examples',
-  mode: webpackMode,
-  entry: {
-    string: './examples/string.js',
-    file: './examples/file.js',
-  },
-  output: {
-    path: dist,
-    filename: 'example_[name].js',
-  },
-  experiments: {
-    syncWebAssembly: true,
-    topLevelAwait: true,
-  },
-  resolve: {
-    fallback: {
-      https: require.resolve('https-browserify'),
-      http: require.resolve('stream-http'),
-      url: require.resolve('url/'),
-      util: require.resolve('util/'),
-      events: false,
+    name: 'examples',
+    mode: webpackMode,
+    entry: {
+        string: './examples/string.js',
+        file: './examples/file.js',
     },
-    modules: [path.resolve(__dirname, 'node_modules')],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-      },
+    output: {
+        path: dist,
+        filename: 'example_[name].js',
+    },
+    experiments: {
+        syncWebAssembly: true,
+        topLevelAwait: true,
+    },
+    resolve: {
+        fallback: {
+            https: require.resolve('https-browserify'),
+            http: require.resolve('stream-http'),
+            url: require.resolve('url/'),
+            util: require.resolve('util/'),
+            events: false,
+        },
+        modules: [path.resolve(__dirname, 'node_modules')],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+        ],
+    },
+    plugins: [
+        new webpack.ProvidePlugin({
+            process: 'process/browser',
+        }),
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            filenameme: 'example_string.html',
+            chunks: ['string'],
+        }),
+        new HtmlWebpackPlugin({
+            filename: 'example_file.html',
+            template: './examples/file.html',
+            chunks: ['file'],
+        }),
     ],
-  },
-  plugins: [
-    new webpack.ProvidePlugin({
-      process: 'process/browser',
-    }),
-    new CleanWebpackPlugin({
-      cleanOnceBeforeBuildPatterns: ['example_**'],
-    }),
-    new HtmlWebpackPlugin({
-      filename: 'example_string.html',
-      chunks: ['string'],
-    }),
-    new HtmlWebpackPlugin({
-      filename: 'example_file.html',
-      template: './examples/file.html',
-      chunks: ['file'],
-    }),
-  ],
 }
